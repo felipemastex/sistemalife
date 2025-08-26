91899646c41fb4b4ee8a9d8fa29cc2c12b5a0edd
Felipe <felipemastex@gmail.com>
2025-08-26 19:28:30 -0300

    quase perdi minha fonte

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,18 @@ const nextConfig = {
   },
   // Allow development origins for network access
   allowedDevOrigins: ['http://192.168.1.21:3002', 'http://localhost:3002'],
+  
+  // Add this to serve static font files
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/fonts/[name].[hash][ext]'
+      }
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
