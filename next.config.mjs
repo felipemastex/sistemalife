7290c548b0caaad4866b1d1ce02b455ff2ba72cc
felipe silva <felipe.fwq@gmail.com>
2025-08-28 12:14:33 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  // Allow development origins for network access
-  allowedDevOrigins: ['http://192.168.1.21:3002', 'http://localhost:3002'],
-  
-  // Add this to serve static font files
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/fonts/[name].[hash][ext]'
-      }
-    });
+  webpack: (config, { isServer }) => {
+    // Adicionado para corrigir o erro 'require.extensions is not supported by webpack'
+    // que vem da dependência 'handlebars' no genkit.
+    // Isto diz ao webpack para não empacotar o 'handlebars' no lado do servidor.
+    if (isServer) {
+      config.externals = [...config.externals, 'handlebars'];
+    }
+
     return config;
-  }
+  },
 };
 
 export default nextConfig;
